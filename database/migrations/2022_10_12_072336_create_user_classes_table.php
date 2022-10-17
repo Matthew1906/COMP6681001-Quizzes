42b8f5460f6908201f5435af064c45eeaa5ee45f
AkuwuTumapel <70062072+AkuwuTumapel@users.noreply.github.com>
2022-10-17 11:31:34 +0700

    Declared variables and primary keys correctly

--- database/migrations/2022_10_12_072336_create_user_classes_table.php
+++ database/migrations/2022_10_12_072336_create_user_classes_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('user_classes', function (Blueprint $table) {
-            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("class_id");
+            $table->primary(["user_id", "class_id"]);
             $table->foreign("user_id")->references("id")->on("users")->onUpdate("cascade")->onDelete("cascade");
             $table->foreign("class_id")->references("id")->on("class_groups")->onUpdate("cascade")->onDelete("cascade");
             $table->timestamps();
